84811a97b6444729540aebaa302772c34cda5769
Arturo Silva <arturo.silva@washpost.com>
2024-06-17 15:02:53 -0400

    fix: import to kit rather than theme

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import "./../src/nextra.css";
 import React from "react";
-import { getCssText, globalStyles } from "@washingtonpost/wpds-theme";
+import { getCssText, globalStyles } from "@washingtonpost/wpds-ui-kit";
 
 export default function Nextra({ Component, pageProps }) {
   // global styles adds wpds fonts
